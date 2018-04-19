cbdcabd5927f7664d6fca82cff3374f5fe9bda60
Matt Rota <mrota@stevens.edu>
2018-04-19 11:13:39 -0400

    updated filetree constructors

--- filetree.h
+++ filetree.h
@@ -6,7 +6,8 @@
 #define FILESYSTEMS_NODE_H
 
 #include <string>
-#include <utility> #include <vector>
+#include <utility>
+#include <vector>
 
 class FileTree {
     std::string path, name;
@@ -15,12 +16,19 @@ class FileTree {
 public:
     FileTree* parent;
     std::vector<FileTree*> children;
-    FileTree(std::string, FileTree*);                             // Constructor: directory node
     FileTree(std::string, FileTree*, std::string, off_t, time_t); // Constructor: file node
+
+    // Getters
+    std::string get_path() { return path; }
+    std::string get_name() { return name; }
+    off_t get_size() { return size; }
+    time_t get_timestamp() { return timestamp; }
 
     int add_child(FileTree*);
     unsigned long num_child() { return children.size(); }
     void update_timestamp(time_t new_timestamp) { timestamp = new_timestamp; }
+    // remove_child()
+    FileTree* get_node(std::string, std::string);
 };
 
 // Class member declarations
@@ -39,5 +47,14 @@ int FileTree::add_child(FileTree *Child) {
     return 1;
 }
 
+FileTree* FileTree::get_node(std::string p, std::string n=NULL) {
+    FileTree * temp = this;
+    if (n == NULL) {
+        // Node returned will be a directory
+        if (get_path() == p) {
+            return temp;
+        }
+    }
+}
 
 #endif //FILESYSTEMS_NODE_H
